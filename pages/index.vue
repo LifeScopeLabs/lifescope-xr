066b4416d046c6be2c83136be0df0e0ebd779dcf
mohrtw <mohrtw@gmail.com>
2018-04-18 14:30:27 -0700

    added index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1><a href="/vrgallery">Gallery</a></h1>
+    <h1><a href="/vrmap">VR Map</a></h1>
+    <h1><a href="/armap">AR Map</a></h1>
+  </div>
 </template>
