dc10f6eb0f35e2b367428cf038fcc36e02d58b8a
mohrtw <mohrtw@gmail.com>
2018-04-23 16:30:22 -0700

    added data to canavasC

--- components/canvasC.vue
+++ components/canvasC.vue
@@ -1,9 +1,19 @@
 <template>
-    <a-entity>
+    <a-entity event-set__click="testMethod">
         <wallimage :image="imageC"
                    position="0 1 0"/>
+        
+
         <xrtext :textValue="textValueC"
-                   position="0 -.2 0"/>
+                position="0 -.2 0"/>
+
+
+        <xrtext :textValue="imageC.connection"
+                   position="0 -.4 0"/>
+        <xrtext :textValue="imageC.updated"
+                   position="0 -.6 0"/>
+        <xrtext :textValue="imageC.homepoint"
+                   position="0 -.8 0"/>
     </a-entity>
     
 </template>
@@ -15,10 +25,15 @@ import wallimage from "../components/wallimage.vue"
 
 console.log("from canvas.vue <script>")
 export default {
-    props: ['imageC', 'textValueC'],
+    props: ['imageC', 'textValueC', 'connection'],
     components: {
         xrtext,
         wallimage
+    },
+    methods: {
+        testMethod: function (event) {
+            conosle.log("testMethod called")
+        }
     }
   }
 </script>
