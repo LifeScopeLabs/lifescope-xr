e8fa65ad451a113281b73c453c5a5da19e0e019b
mohrtw <mohrtw@gmail.com>
2018-04-27 16:46:23 -0700

    creates geojson lines on sphere from latlong values

--- pages/vrgallery.vue
+++ pages/vrgallery.vue
@@ -12,20 +12,15 @@
       </a-assets>
 
       <!-- Geojson -->
-      <a-assets>
+      <a-assets id="geo-assets">
         <a-asset-item id="geojson-simple" src="geodata/simple.geojson" />
       </a-assets>
  
-      
-
       <!-- Load assets with imageLoader -->
       <!-- https://www.pexels.com/search/travel/ -->
-
       <imageLoader v-for="wimage in wallimages"
                    :key="wimage.id"
                    :image="wimage" />
-
-      
 
       <!-- Camera -->
       <a-camera>
@@ -283,16 +278,101 @@ export default {
       }
     },
 
-    injectGeojson : function () {
+    injectGeojsonSimple : function () {
         var sceneEl = document.querySelector('a-scene');
 
         sceneEl.addEventListener('loaded', function () {
-          console.log("injectGeojson")
+          console.log("injectGeojsonSimple")
           // add GeoJson.  
           var aContainer = sceneEl.querySelector('#globe-container')
           aContainer.innerHTML = '<a-entity id="globe" geometry="primitive: sphere; radius: 1;" material="color: #F0A;" geojson="src: #geojson-simple; featureKey: name;"></a-entity>';
         });
+    },
+
+    injectGeojson : function (src) {
+      // inject a-entity of a globe with geojson from src
+      var sceneEl = document.querySelector('a-scene');
+
+      sceneEl.addEventListener('loaded', function () {
+        console.log("injectGeojsonTest")
+        // add GeoJson.  
+        var aContainer = sceneEl.querySelector('#globe-container')
+        aContainer.innerHTML = '<a-entity id="globe" geometry="primitive: sphere; radius: 1;" material="color: #F0A;" geojson="src: #' + src + '; featureKey: name;"></a-entity>';
+      });
+    },
+
+    loadGeoAsset : function (geoAsset) {
+      // loads geojson data into assets on the fly
+      console.log("loadGeoAsset")
+
+      var sceneEl = document.querySelector('a-scene');
+      var geosrc = this.geoObjectToUrl(geoAsset)
+      //console.log(geosrc)
+
+      sceneEl.addEventListener('loaded', function () {
+        console.log("loadGeoAsset")
+        
+        // add GeoJson asset  
+        var aAssets = sceneEl.querySelector('#geo-assets')
+        var geoAsset = document.createElement("a-asset-item");
+        geoAsset.setAttribute('id', 'geojson-fly')
+        geoAsset.setAttribute('src', geosrc)//'/simple.geojson')//geosrc)
+
+        //console.log(geoAsset)
+        aAssets.appendChild(geoAsset)
+        //console.log(aAssets)
+      });
+    },
+
+    latlongToGeojsonPoint : function (lat, long) {
+      // creates a geojson point object from lat/long values
+      console.log("latlongToGeojsonPoint")
+      var gj = {"type": "FeatureCollection",
+                "features": []}
+      var feature = {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            lat,
+            long
+          ]
+        }
       }
+      gj.features.push(feature)
+      //console.log(gj)
+      return(gj)
+    },
+
+    latlongToGeojsonLine : function (coordinates) {
+      console.log("latlongToGeojsonLine")
+      // creates a geojson line object from an array of lat/long values
+      // coordinates is an array of lat long values
+      // [[0,1], [300,-20], [0, -20]]
+      var gj = {"type": "FeatureCollection",
+                "features": []}
+      var feature = {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "LineString",
+          "coordinates": coordinates
+        }
+      }
+      gj.features.push(feature)
+      //console.log(gj)
+      return(gj)
+    },
+
+    geoObjectToUrl : function (geo) {
+      // converts a geojson object into a url
+      console.log("geoObjectToUrl")
+      //console.log(geo)
+      var url = 'data:text/json;charset=utf8,' + encodeURIComponent(JSON.stringify(geo));
+      //console.log(url)
+      return(url)
+    }
   
   },
 
@@ -317,7 +397,9 @@ export default {
     this.createOpenLinkOnClickListenerAframeComponent()
 
     // Inject geojson
-    this.injectGeojson()
+    //this.injectGeojson()
+    this.loadGeoAsset(this.latlongToGeojsonLine([[0,1], [300,-20], [0, -20]]))
+    this.injectGeojson('geojson-fly');
 
     // test
     //this.changeTextOnBackwall("Hello from changeTextOnBackwall")
