0cbf1520e3d36a1a53499dbf296cd588da45d7e1
William Broza <lbroza@bitscoop.com>
2018-07-23 15:54:33 -0700

    Added map fix.

--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ import './components/aframe/play-gaze.js';
 import './components/aframe/dynamic-autoplay.js';
 import './components/aframe/entangle.js';
 import './components/aframe/avatar-rig';
+import './components/aframe/mapbox-terrain.js';
 import './components/hubs/character-controller.js';
 import './components/hubs/pitch-yaw-rotator.js';
 
