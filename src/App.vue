f96960c758b2578cb61b4037336abc5a42f5d5a3
mohrtw <mohrtw@gmail.com>
2019-02-27 20:23:25 -0800

    created avatar class

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,8 @@ import easyrtc from '../static/easyrtc/easyrtc.js';
 
 import gallery from "./components/gallery.vue";
 
+import Avatar from "./avatar.js";
+
 var CONFIG = {};
 CONFIG.DEBUG = true;
 import debugListeners from './dev/listeners.js';
@@ -112,16 +114,13 @@ export default {
 
           this.getObjs().then((res) => {
             console.log("getObjs");
-            // console.log(res);
-            // var event = new Event('lsobjs');
-            // document.body.dispatchEvent(event);
             this.LSObjs = res.LSObjs;
-            // console.log(this.LSObjs);
             this.rooms = res.rooms;
 
-            this.createAvatarRigTemplate();
-            this.addAvatarRigTemplate();
-            this.createNetworkedPlayer();
+            var avatar = new Avatar();
+            avatar.createAvatarRigTemplate();
+            avatar.addAvatarRigTemplate();
+            avatar.createNetworkedPlayer();
 
             if (AFRAME.utils.device.isMobile()) {
               if (CONFIG.DEBUG) {console.log("isMobile");};
@@ -145,23 +144,10 @@ export default {
 
 
     methods: {
-      textString: function (value) {
-            return 'width: 1.5; color: white; value: ' + value
-      },
-      logText: function (value) {
-        var logtext = document.getElementById('log-text');
-        logtext.setAttribute('text', this.textString(value));
-      },
-      testButtonAction: function () {
-        if (CONFIG.DEBUG) {console.log("testButtonAction");};
-      },
-      
       getRoomConfig () {
         if (CONFIG.DEBUG) {console.log("getRoomConfig");};
         return axios.get("/roomconfig")
         .then((res) => {
-          // console.log("res.data");
-          // console.log(res.data);
           return {roomConfig: res.data}
         })
       },
@@ -183,150 +169,10 @@ export default {
           var result = [];
           var rooms = Object.keys(res.data);
           var someData = res.data[this.roomName].forEach(element => {
-            // console.log(element);
             result.push(element);
           });
           return { LSObjs: result, rooms: rooms }
         })
-      },
-
-      createAvatarRigTemplate() {
-        var frag = this.fragmentFromString(`
-        <template id="avatar-rig-template" v-pre>
-          <a-entity class="player">
-
-            <a-entity class="avatar" networked-audio-source >
-              <a-sphere class="head"
-                color="#5985ff"
-                scale="0.45 0.5 0.4"
-              ></a-sphere>
-              <a-entity class="face"
-                position="0 0.05 0"
-              >
-                <a-sphere class="eye"
-                  color="#efefef"
-                  position="0.16 0.1 -0.35"
-                  scale="0.12 0.12 0.12"
-                >
-                  <a-sphere class="pupil"
-                    color="#000"
-                    position="0 0 -1"
-                    scale="0.2 0.2 0.2"
-                  ></a-sphere>
-                </a-sphere>
-                <a-sphere class="eye"
-                  color="#efefef"
-                  position="-0.16 0.1 -0.35"
-                  scale="0.12 0.12 0.12"
-                >
-                  <a-sphere class="pupil"
-                    color="#000"
-                    position="0 0 -1"
-                    scale="0.2 0.2 0.2"
-                  ></a-sphere>
-                </a-sphere>
-                <a-sphere class="eye dev"
-                  color="#efefef"
-                  position="0 0.1 0.35"
-                  scale="0.12 0.12 0.12"
-                >
-                  <a-sphere class="pupil dev"
-                    color="#000"
-                    position="0 0 1"
-                    scale="0.2 0.2 0.2"
-                  ></a-sphere>
-                </a-sphere>
-              </a-entity>
-            </a-entity>
-
-          </a-entity>
-        </template> 
-        `);
-
-        document.getElementsByClassName('aframe-assets')[0].appendChild(frag);
-
-      },
-
-      addAvatarRigTemplate() {
-        if (CONFIG.DEBUG) {console.log("addAvatarRigTemplate");};
-        NAF.schemas.add({
-          template: '#avatar-rig-template',
-          components: [
-            {
-              component: 'position'
-            },
-            {
-              component: 'rotation'
-            },
-            {
-              selector: '.avatar',
-              component: 'rotation'
-            }
-          ]
-       });
-      },
-
-
-          // <a-entity id="cursor"
-          //       cursor="fuse: true; fuseTimeout: 500"
-          //       position="0 0 -1"
-          //       geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
-          //       material="color: black; shader: flat">
-          //     </a-entity>
-
-      // avatar-rig="camera:#player-camera;"
-      // pitch-yaw-rotator
-      // look-controls="reverseMouseDrag:true"
-      
-      // <a-gui-cursor id="cursor"
-			// 		  raycaster="interval: 1000; objects: gui-interactable, .clickable"
-			// 		  fuse="true" fuse-timeout="2000"
-			// 		  design="dot"
-			//       >
-			//     </a-gui-cursor>
-      createNetworkedPlayer() {
-        var frag = this.fragmentFromString(`
-        <a-entity id="playerRig"
-          position="0 1.6 -2"
-          wasd-controls
-          networked="template:#avatar-rig-template;attachTemplateToLocal:true;"
-          
-          character-controller="pivot: #player-camera"
-          >
-         
-         <a-entity id="player-camera"
-            class="camera"
-            camera
-            pitch-yaw-rotator
-          >
-          
-          
-          
-          </a-entity>
-          <a-entity id="rightHandRig"
-            class="hand"
-          ></a-entity>
-        </a-entity>`);
-        document.getElementsByTagName('a-scene')[0].appendChild(frag);
-      },
-
-
-      createRightHandNetworked() {
-        var frag = this.fragmentFromString(`
-        <a-entity id="rightHandController"
-           teleport-controls="cameraRig: #playerRig; teleportOrigin: #player-camera; startEvents: teleportstart; endEvents: teleportend; collisionEntities:.boundry; landingNormal: 0 0 1;"
-            daydream-controls="hand: right;"
-            oculus-touch-controls="hand: right"
-            vive-controls="hand: right"
-            windows-motion-controls="hand: right"
-            gearvr-controls="hand: right"
-            oculus-go-controls="hand: right">
-         </a-entity>`);
-        document.getElementById('playerRig').appendChild(frag);
-      },
-
-      fragmentFromString(strHTML) {
-            return document.createRange().createContextualFragment(strHTML);
       },
 
     }
